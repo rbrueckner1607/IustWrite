25c504a82bc5a07238770b8e8b3fcee2f31a7e8d
rbrueckner1607 <robert.brueckner.burg@gmail.com>
2026-02-04 21:28:37 +0100

    app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def main():
 \usepackage[utf8]{inputenc}
 \usepackage{setspace}
 \usepackage[T1]{fontenc}
-\usepackage{fouriernc}
+\usepackage{baskervaldx}
 \usepackage{geometry}
 \usepackage{fancyhdr}
 \usepackage{tocloft}
