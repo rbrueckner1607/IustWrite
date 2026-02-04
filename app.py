bfdcf74d73ef16a53e0874ae19ee216ef761331e
rbrueckner1607 <robert.brueckner.burg@gmail.com>
2026-02-04 21:27:03 +0100

    app.py

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def main():
 \usepackage[utf8]{inputenc}
 \usepackage{setspace}
 \usepackage[T1]{fontenc}
-\usepackage{ebgaramond}
+\usepackage{fouriernc}
 \usepackage{geometry}
 \usepackage{fancyhdr}
 \usepackage{tocloft}
