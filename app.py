510c0156fa20f283fbab23d35ffe5ee39752ecba
rbrueckner1607 <robert.brueckner.burg@gmail.com>
2026-02-05 22:24:29 +0100

    app.py

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ def main():
 \usepackage[ngerman]{babel}
 \usepackage[utf8]{inputenc}
 \usepackage[T1]{fontenc}
-\usepackage{helvet} \renewcommand\familydefault{\sfdefault}
+\usepackage{Palantino}
 \usepackage{setspace}
 \usepackage{geometry}
 \usepackage{fancyhdr}
