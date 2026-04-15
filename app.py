3c6524ad13e3046a0a94b7c18632048d79a721c6
rbrueckner1607 <robert.brueckner.burg@gmail.com>
2026-04-15 17:17:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ def main():
 
         with tab_gliederung:
             st.markdown("### 2. Die 9 Gliederungsebenen")
-            st.write("Eingaben werden automatisch als Überschriften erkannt und korrekt formatiert. Bitte verwende konsequent die standardmäßige alphanumerische Gliederungslogik. Beginne bei einer neuen Überschrift einfach in einer jeweils neuen Zeile, um die Hierarchie zu steuern. Bitte achte bei den Gliederungspunkten darauf, die Punkte und Klammern richtig zu setzen (z.B. Aufgabe 1`.` oder aa`)`. Wenn du einen neuen Gliederungspunkt eingegeben hast erscheint dieser nicht immer sofort links in der Gliederungs-Sidebar. Drücke dafür einfach `Cmd + Enter` bzw. `Strg + Enter` oder klicke neben das Editorfenster.")
+            st.write("Eingaben werden automatisch als Überschriften erkannt und korrekt formatiert. Bitte verwende konsequent die standardmäßige alphanumerische Gliederungslogik. Beginne bei einer neuen Überschrift einfach in einer jeweils neuen Zeile, um die Hierarchie zu steuern. Bitte achte bei den Gliederungspunkten darauf, die Punkte und Klammern richtig zu setzen (z.B. Aufgabe 1`.` oder aa`)`. Wenn du einen neuen Gliederungspunkt eingegeben hast, erscheint dieser manchmal nicht automatisch links in der Gliederungs-Sidebar. Drücke dafür einfach `Cmd + Enter` bzw. `Strg + Enter` oder klicke neben das Editorfenster.")
             st.markdown("""
             | Ebene | Kürzel / Beispiel | Typ |
             | :--- | :--- | :--- |
